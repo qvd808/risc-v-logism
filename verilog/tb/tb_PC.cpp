f625e002a6b5416b0cfbe6e2b72ddc2f3c320950
Quang Vinh Dang <dqvinh101@gmail.com>
2026-03-06 15:22:14 -0800

    Fix minor unit test on PC

--- verilog/tb/tb_PC.cpp
+++ verilog/tb/tb_PC.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
   tb.dut->areset = 0;
   tb.dut->eval();
   tb.half_tick(CLK_PERIOD);
-  tb.check_hex("PC load on start", tb.dut->PC_Address, 0x00000008);
+  tb.check_hex("PC load on start", tb.dut->PC_Address, 0x00000008, 50);
 
   tb.dut->PCEnable = 1;
   tb.dut->areset = 1;
@@ -22,14 +22,14 @@ int main(int argc, char **argv) {
   tb.dut->load_address = 1;
   tb.half_tick(CLK_PERIOD);
   tb.check_hex("Should not load on failing edge", tb.dut->PC_Address,
-               0x00000000);
+               0x00000000, 50);
 
   tb.dut->PCEnable = 1;
   tb.dut->areset = 0;
   tb.dut->address_to_load = 0x12;
   tb.dut->load_address = 0;
   tb.half_tick(CLK_PERIOD);
-  tb.check_hex("PC + 4 ", tb.dut->PC_Address, 0x00000004);
+  tb.check_hex("PC + 4 ", tb.dut->PC_Address, 0x00000004, 50);
 
   tb.dut->PCEnable = 1;
   tb.dut->areset = 0;
@@ -37,14 +37,14 @@ int main(int argc, char **argv) {
   tb.dut->load_address = 0;
   tb.half_tick(CLK_PERIOD);
   tb.check_hex("PC + 4 failling edge should not update", tb.dut->PC_Address,
-               0x00000004);
+               0x00000004, 50);
 
   tb.dut->PCEnable = 1;
   tb.dut->areset = 0;
   tb.dut->address_to_load = 0x12;
   tb.dut->load_address = 0;
   tb.tick(CLK_PERIOD);
-  tb.check_hex("PC + 8 ", tb.dut->PC_Address, 0x00000008);
+  tb.check_hex("PC + 8 ", tb.dut->PC_Address, 0x00000008, 50);
 
   tb.dut->PCEnable = 0;
   tb.dut->areset = 0;
@@ -52,14 +52,14 @@ int main(int argc, char **argv) {
   tb.dut->load_address = 0;
   tb.tick(CLK_PERIOD);
   tb.check_hex("PC should not update due to not Enable", tb.dut->PC_Address,
-               0x00000008);
+               0x00000008, 50);
 
   tb.dut->PCEnable = 1;
   tb.dut->areset = 0;
-  tb.dut->address_to_load = 0x12;
+  tb.dut->address_to_load = 0x24;
   tb.dut->load_address = 1;
   tb.tick(CLK_PERIOD);
-  tb.check_hex("PC jumps to 0x12", tb.dut->PC_Address, 0x00000012);
+  tb.check_hex("PC jumps to 0x24", tb.dut->PC_Address, 0x00000024, 50);
 
   tb.show_summary();
 
